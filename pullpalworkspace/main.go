032288de59d347b08c07dc7c34ac43914744b81a
black-crowned-night-heron <mobys.nycticorax@gmail.com>
2023-05-12 13:06:37 -0400

    update based on comment

--- pullpalworkspace/main.go
+++ pullpalworkspace/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/nsf/termbox-go"
 	"time"
 )
 
 func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
 	fmt.Println("Welcome to Download Simulator 2D Platform Game!")
 
 	// Define core physics variables
@@ -21,12 +28,37 @@ func main() {
 	}
 	player := Player{0, 0, 0, 0, true, false}
 
+	// Set up player input
+	go func() {
+		for {
+			event := termbox.PollEvent()
+			if event.Type == termbox.EventKey {
+				playerInput(event.Key, &player, jumpForce)
+			}
+		}
+	}()
+
 	// Create a function to simulate a game loop
 	gameLoop := time.Tick(time.Millisecond * 16)
 
 	for range gameLoop {
 		updateGame(&player, gravity, momentum, jumpForce)
 		renderGame(player)
+	}
+}
+
+func playerInput(key termbox.Key, player *Player, jumpForce float32) {
+	switch key {
+	case termbox.KeyArrowLeft, termbox.KeyCtrlB:
+		player.velocityX -= 5
+	case termbox.KeyArrowRight, termbox.KeyCtrlF:
+		player.velocityX += 5
+	case termbox.KeyArrowUp, termbox.KeySpace, termbox.KeyCtrlP:
+		if player.isOnGround {
+			player.velocityY = -jumpForce
+			player.isOnGround = false
+			player.isJumping = true
+		}
 	}
 }
 
